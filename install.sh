2c7febc129f76f342e8d892c94b031ab2a5922c2
Tom Jacobs <hatsmagee@gmail.com>
2025-08-11 07:14:50 +1000

    fix the goddamn broken can package by downgrading

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
-pip install python-can[serial]
+#pip install python-can[serial]
+python3 -m pip install --user "python-can==4.4.2"
+
+# Allow access to serial port
+sudo usermod -aG dialout $USER
+newgrp dialout
+groups
+ls -l /dev/ttyACM0
 
