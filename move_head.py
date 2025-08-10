7be989df4f83a84bb1c44a8a2ea1b16212a973e8
Tom Jacobs <hatsmagee@gmail.com>
2025-08-11 06:44:49 +1000

    install

--- move_head.py
+++ move_head.py
@@ -7,4 +7,22 @@ CAN_PORT = "/dev/tty.usbmodem206F308B35411"
 motor_names = ['head_yaw', 'head_pitch']
 motor_ids = [0x10, 0x11]
 
+# Global state
+motor_port = None
+motors = {}
+
+# Run
+def main():
+    global motor_port, motors
+
+    init()
+    
+
+def init():
+    print("Init motors")
+
+
+# Run main
+if __name__ == "__main__":
+    main()
 
