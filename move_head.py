378c5f96d2755ddf42a4dfcfaa0f4392adb32b31
Tom Jacobs <hatsmagee@gmail.com>
2025-08-11 06:48:17 +1000

    init motors

--- move_head.py
+++ move_head.py
@@ -25,6 +25,26 @@ def main():
 def init():
     print("Init motors")
 
+    # Init CAN
+    motor_port = Motors(channel=CAN_PORT)
+
+    # Create map of motor names to motors
+    initialized_motor_count = 0
+    for name, motor_id in zip(motor_names, motor_ids):
+        if motor_port.ping_motor(motor_id):
+            motors[name] = motor_port.init_motor(motor_id)
+            initialized_motor_count += 1
+            time.sleep(0.01)
+            print(f"{name} motor with ID 0x{motor_id:02x} initialized")
+        else:
+            print(f"*** Warning: Could not find motor {name} with ID 0x{motor_id:02x}")
+
+    # Check if all expected motors were initialized
+    total_expected_motors = len(motor_names)
+    print(f"\nMotors initialized: {initialized_motor_count} / {total_expected_motors}")
+    if initialized_motor_count != total_expected_motors:
+        print(f"WARNING: Not all expected motors were initialized! {initialized_motor_count} out of {total_expected_motors}.")
+
 
 # Run main
 if __name__ == "__main__":
